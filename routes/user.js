69997a66fb49fef2461889da5646c14f03b15bec
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-06-16 11:28:25 +0700

    16-6-24 (select all query)

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,16 @@
 const express = require('express'), router = express.Router();
 
 router.get('/list', (req, res) => {
-    res.send("<h1>List Page</h1>")
-})
+    let query = "SELECT * FROM users";
+    db.query(query, (err, result, fields) => {
+        if (err) throw err;
+        res.json({
+            status: 200,
+            result ,
+            message: "user list obtained successfully"
+        });
+    });
+});
 
 
 module.exports = router;
