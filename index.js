06183d88d2dbd07b431d3a942f017dce6345c1ab
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-06-21 20:39:49 +0700

    21-6-24 (adding user)
    done
    -adding some user to database
    -now you can post your user to database using insomnia

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require('express'), app = express(), mysql = require('mysql'), cors = require('cors'), bodyParser = require('body-parser'), userRouter = require('./routes/user');
+const express = require('express'), app = express(), cors = require('cors'), bodyParser = require('body-parser'), userRouter = require('./routes/user');
 var server = {
     port: 8080
 };
-var db = mysql.createConnection({
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'my_db',
-})
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/user', userRouter);
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,11 @@
-const express = require('express'), router = express.Router();
+const express = require('express'), router = express.Router(), mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'my_db',
+});
 
 router.get('/list', (req, res) => {
     let query = "SELECT * FROM users";
@@ -11,6 +18,22 @@ router.get('/list', (req, res) => {
         });
     });
 });
+
+router.post('/new', (req, res) => {
+    let query = "INSERT INTO users(name, gender) values(?)";
+    let values = [
+        req.body.name,
+        req.body.gender
+    ];
+    db.query(query, [values], (err, result, fields) => {
+        if (err) throw err;
+        res.json({
+            status : 200,
+            message: "user added successfully"
+        });
+    });
+});
+
 
 
 module.exports = router;
