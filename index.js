789946950a0cb8aade21d11809071d89f97e530c
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-06-28 20:20:21 +0700

    28-6-24 (template for res api)
    completed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express'), app = express(), cors = require('cors'), bodyParser = require('body-parser'), userRouter = require('./routes/user');
+const express = require('express'), app = express(), cors = require('cors'), bodyParser = require('body-parser'), userRouter = require('./routes/user'), path = require('path');
 var server = {
     port: 8080
 };
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter);
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "/index.html"));
+})
 app.listen(server.port, () => console.log('listening on port ' + server.port));
